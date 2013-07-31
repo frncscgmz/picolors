f399dfea7c22e88fa76264a037f5ca460d02ce93
Francisco Gomez <fgleon@serviciostmx.com>
2013-07-31 13:32:12 -0700

    Nueva forma para subir imagenes, extensiones permitidas

--- picolors.py
+++ picolors.py
@@ -1,13 +1,24 @@
 from __future__ import with_statement
+import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
       render_template, flash, _app_ctx_stack
+from werkzeug import secure_filename
 
-# configuration
+# configuracion
 DEBUG = True
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('PICOLOR_SETTINGS', silent=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# archivos permitidos
+def allowed_file(filename):
+   return '.' in filename and \
+         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 def show_index():
