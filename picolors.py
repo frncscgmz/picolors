79ac8b38cf6dce034ff0a0350c3d00de3c1b9de5
Francisco Gomez <fgleon@serviciostmx.com>
2013-07-31 17:59:07 -0700

    Nuevos metodos para subir y desplegar la imagen subida

--- picolors.py
+++ picolors.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-      render_template, flash, _app_ctx_stack
+      render_template, flash, _app_ctx_stack, send_from_directory
 from werkzeug import secure_filename
 
 # configuracion
@@ -23,6 +23,21 @@ def allowed_file(filename):
 @app.route('/')
 def show_index():
    return render_template('index.html')
+
+@app.route('/',methods=['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      file = request.files['file']
+      if file and allowed_file(file.filename):
+         filename = secure_filename(file.filename)
+         file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+         return redirect(url_for('uploaded_file',
+            filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+   return send_from_directory(app.config['UPLOAD_FOLDER'],
+         filename)
 
 if __name__=='__main__':
    app.run()
