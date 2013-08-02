35fdad109a7df68e37195d50aec96a25c86aa64e
Francisco Gomez <fgleon@serviciostmx.com>
2013-08-01 17:33:39 -0700

    Metodos para obtener colores dominantes agregados

--- picolors.py
+++ picolors.py
@@ -4,6 +4,14 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
       render_template, flash, _app_ctx_stack, send_from_directory
 from werkzeug import secure_filename
 
+from collections import namedtuple
+from math import sqrt
+import random
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
 # configuracion
 DEBUG = True
 UPLOAD_FOLDER = '/tmp'
@@ -13,6 +21,9 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('PICOLOR_SETTINGS', silent=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+Point = namedtuple('Point', ('coords', 'n', 'ct'))
+Cluster = namedtuple('Cluster', ('points', 'center', 'n'))
 
 # archivos permitidos
 def allowed_file(filename):
@@ -44,8 +55,69 @@ def uploaded_file(filename):
 @app.route('/show/<filename>')
 def show_file(filename):
    image = "http://127.0.0.1:5000/uploads/"+filename
-   print image
+   image_local = UPLOAD_FOLDER+"/"+filename
+   colormap = colorz(image_local)
+   for key in colormap:
+      print key
    return render_template('view_image.html',image=image)
+
+# obtener 3 colores principales de imagen
+def colorz(filename, n=3):
+    img = Image.open(filename)
+    img.thumbnail((200, 200))
+    w, h = img.size
+
+    points = get_points(img)
+    clusters = kmeans(points, n, 1)
+    rgbs = [map(int, c.center.coords) for c in clusters]
+    return map(rtoh, rgbs)
+
+# obtener puntos en la imagen
+def get_points(img):
+    points = []
+    w, h = img.size
+    for count, color in img.getcolors(w * h):
+        points.append(Point(color, 3, count))
+    return points
+
+rtoh = lambda rgb: '#%s' % ''.join(('%02x' % p for p in rgb))
+
+def kmeans(points, k, min_diff):
+    clusters = [Cluster([p], p, p.n) for p in random.sample(points, k)]
+    while 1:
+        plists = [[] for i in range(k)]
+        for p in points:
+            smallest_distance = float('Inf')
+            for i in range(k):
+                distance = euclidean(p, clusters[i].center)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    idx = i
+            plists[idx].append(p)
+        diff = 0
+        for i in range(k):
+            old = clusters[i]
+            center = calculate_center(plists[i], old.n)
+            new = Cluster(plists[i], center, old.n)
+            clusters[i] = new
+            diff = max(diff, euclidean(old.center, new.center))
+        if diff < min_diff:
+            break
+    return clusters
+
+def euclidean(p1, p2):
+    return sqrt(sum([
+        (p1.coords[i] - p2.coords[i]) ** 2 for i in range(p1.n)
+    ]))
+
+def calculate_center(points, n):
+    vals = [0.0 for i in range(n)]
+    plen = 0
+    for p in points:
+        plen += p.ct
+        for i in range(n):
+            vals[i] += (p.coords[i] * p.ct)
+    return Point([(v / plen) for v in vals], n, 1)
 
 if __name__=='__main__':
    app.run()
